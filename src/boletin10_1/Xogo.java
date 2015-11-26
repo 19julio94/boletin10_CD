cefbe9f350672673a9cef77392c3b98b2a515545
jpatriciodasilva <jpatriciodasilva@atanasoff06.danielcastelao.org>
2015-11-26 09:23:37 +0100

    sin metodo

--- src/boletin10_1/Xogo.java
+++ src/boletin10_1/Xogo.java
@@ -8,20 +8,27 @@ import javax.swing.JOptionPane;
  */
 public class Xogo {
 
-    public void adivinaNumero(int num) {
+    public void adivinaNumero(int num, int numIntentos) {
         boolean respuesta = false;
+        int cont = 0;
         while (respuesta != true) {
+            if (cont == numIntentos) {
+                JOptionPane.showMessageDialog(null, "Numero de intentos superado");
+                respuesta = true;
+                break;
+            }
             int res = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero"));
-            if(res<num){
+            if (res < num) {
                 JOptionPane.showMessageDialog(null, "Es mayor");
             }
-            if(res>num){
-                  JOptionPane.showMessageDialog(null, "Es menor");
+            if (res > num) {
+                JOptionPane.showMessageDialog(null, "Es menor");
             }
             if (res == num) {
                 respuesta = true;
                 JOptionPane.showMessageDialog(null, "has acertado");
             }
+            cont++;
         }
     }
 
