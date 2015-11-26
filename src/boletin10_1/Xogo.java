cddc4bab8042080d392a00c8b585e1588c38161f
jpatriciodasilva <jpatriciodasilva@atanasoff06.danielcastelao.org>
2015-11-26 09:58:01 +0100

    con metodo pero sin motodo de introducir datos

--- src/boletin10_1/Xogo.java
+++ src/boletin10_1/Xogo.java
@@ -19,10 +19,12 @@ public class Xogo {
             }
             int res = Integer.parseInt(JOptionPane.showInputDialog("Introduce numero"));
             if (res < num) {
-                JOptionPane.showMessageDialog(null, "Es mayor");
+                imprimir("Es mayor",0);
+                imprimir("Es mayor",1);
             }
             if (res > num) {
-                JOptionPane.showMessageDialog(null, "Es menor");
+                imprimir( "Es menor",0);
+                imprimir("Es mayor",1);
             }
             if (res == num) {
                 respuesta = true;
@@ -31,5 +33,14 @@ public class Xogo {
             cont++;
         }
     }
+public void imprimir(String cadenaAImprimir,int opcion){
+
+    switch (opcion){
+        case 0:System.out.println(cadenaAImprimir);
+            
+        case 1: JOptionPane.showMessageDialog(null, cadenaAImprimir);
+            
+    }
+}
 
 }
